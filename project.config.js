962d3bf43352b32481b31cee817b75cf5681106f
LABCAT <shane.watters.nz@gmail.com>
2025-04-06 16:41:11 +1200

    Repo setup.

---
+++ project.config.js
@@ -0,0 +1,5 @@
+export default {
+  title: "Rectangles No. 9",
+  gtmId: "", 
+  favicon: "https://labcat.nz/favicon.ico"
+}
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import p5 from "p5";
+import "p5/lib/addons/p5.sound";
+import '@lib/p5.polygon.js';
+import './style.scss';
+
+import PatternsNo8 from './PatternsNo8';
+new p5(PatternsNo8);
